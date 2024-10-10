2b1fccc3c6e1da7710a04f354080045fe96f8b89
levin-test <levin@var.jp>
2024-10-11 00:51:45 +0900

    Enhanced update_all.py to automate handling of yay and paru package managers.

--- python_scripts/update_all.py
+++ python_scripts/update_all.py
@@ -5,14 +5,33 @@ BLUE = "\033[1;34m"
 NC = "\033[0;0m"
 
 
+def is_command_exists(command: str):
+    try:
+        subprocess.run(
+            [command, "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+        return True
+    except FileNotFoundError:
+        return False
+
+
 def run_command(cmd):
     print(f"{BLUE}:: {cmd}によるアップデートを実行中...{NC}")
     subprocess.run(cmd, shell=True)
 
 
 if __name__ == "__main__":
-    run_command("paru -Syu")
+    if is_command_exists("yay"):
+        run_command("yay -Syu")
+    elif is_command_exists("paru"):
+        run_command("paru -Syu")
+    else:
+        run_command("sudo pacman -Syu")
+
+    sleep(1)
     run_command("flatpak update")
+    sleep(1)
     run_command("nvim --headless '+Lazy! sync' +qa")
+    sleep(1)
 
     print("\n" + f"{BLUE}:: すべてのアップデートが完了しました{NC}")
