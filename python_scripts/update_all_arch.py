af3c85883195912442664f190d2e9c8376d703c1
levin-test <levin@var.jp>
2024-12-21 03:15:38 +0900

    improve appearance of update_all_arch.py

--- python_scripts/update_all_arch.py
+++ python_scripts/update_all_arch.py
@@ -1,38 +1,43 @@
 import subprocess
-from time import sleep
+import time
 
 BLUE = "\033[1;34m"
 NC = "\033[0;0m"
 
 
 def is_command_exists(command: str):
+    devnull = subprocess.DEVNULL
     try:
-        subprocess.run(
-            [command, "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-        )
+        subprocess.run([command, "--version"], stdout=devnull, stderr=devnull)
         return True
     except FileNotFoundError:
         return False
 
 
-def run_command(cmd, sec=0):
-    print(f"{BLUE}:: {cmd}によるアップデートを実行中...{NC}")
-    sleep(sec)
+def run_command(cmd, sec=0.5, COLOR="\033[1;34m", NC="\033[0;0m"):
+    def show_progress(word):
+        for char in word:
+            print(f"{COLOR}{char}{NC}", end="", flush=True)
+            time.sleep(sec)
+        print(f"{COLOR}!{NC}")
+
+    print(f"{COLOR}:: {cmd}によるアップデートを実行中{NC}", end="")
+    show_progress(".....")
     subprocess.run(cmd, shell=True)
 
 
 if __name__ == "__main__":
     if is_command_exists("yay"):
-        run_command("yay -Syu")
+        run_command("yay -Syu", 0.3, BLUE, NC)
     elif is_command_exists("paru"):
-        run_command("paru -Syu")
+        run_command("paru -Syu", 0.3, BLUE, NC)
     else:
-        run_command("sudo pacman -Syu")
+        run_command("sudo pacman -Syu", 0.3, BLUE, NC)
 
     if is_command_exists("flatpak"):
-        run_command("flatpak update", 3)
+        run_command("flatpak update", 0.3, BLUE, NC)
 
     if is_command_exists("nvim"):
-        run_command("nvim --headless '+Lazy! sync' +qa", 3)
+        run_command("nvim --headless '+Lazy! sync' +qa", 0.3, BLUE, NC)
 
     print("\n" + f"{BLUE}:: すべてのアップデートが完了しました{NC}")
